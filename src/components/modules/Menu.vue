3205aa49408dd06c45785d3fa693393fa8e14069
Janos Perge <janos.perge@gmail.com>
2025-01-16 18:10:37 +0100

    work on menu

--- src/components/modules/Menu.vue
+++ src/components/modules/Menu.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="header-menu">
-    <ul>
+  <div class="header-menu-module hidden lg:block">
+    <ul class="header-menu flex">
       <li v-for="item in menu" :key="item.id">
-        <a :href="item.href">{{ item.title }}</a>
+        <a :href="item.href" class="header-menu-item">{{ item.title }}</a>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
-import moduleData from '../../mock-data/menu.json'
+import moduleData from '@/mock-data/menu.json'
 import { onMounted, ref } from 'vue'
 
 const menu = ref([])
@@ -19,4 +19,38 @@ onMounted(() => {
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+.header-menu {
+  gap: 10px;
+  font-size: 0.875rem;
+  text-transform: uppercase;
+  color: $body-text-color;
+
+  &-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem 1rem;
+    position: relative;
+
+    &:after {
+      content: '';
+      position: absolute;
+      width: 0%;
+      height: 2px;
+      bottom: 5px;
+      left: 0;
+      background-color: $secondary-color;
+      transition: width 0.3s ease-in-out;
+    }
+
+    &:hover {
+      color: $secondary-color;
+      &:after {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
